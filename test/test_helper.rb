10a3d85f9fc3a6aefaa6e6fea5d7eafa6ea4cb12
Ken Collins <ken@metaskills.net>
2015-06-20 14:01:01 -0400

    Scenario 2: Explicit Multi-DB Transactions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'test_helper/sql_helper'
+require 'test_helper/transaction_helper'
 
 class ActiveSupport::TestCase
 
