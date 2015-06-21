edbcefcdf616f13fa05c3e512d7966c0da153f3d
Ken Collins <ken@metaskills.net>
2015-06-21 13:58:11 -0400

    Spacing.

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,18 +1,21 @@
 Delayed::Backend::ActiveRecord::Job.class_eval do
-
   class << self
+
     def connection_pool
       MysqlBase.connection_pool
     end
+
     def retrieve_connection
       MysqlBase.retrieve_connection
     end
+
     def connected?
       MysqlBase.connected?
     end
+
     def remove_connection(klass = self)
       MysqlBase.remove_connection
     end
-  end
 
+  end
 end
