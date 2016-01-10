cb1cd885e6f17be2d0b31dd99517ed2af33faf3f
Ken Collins <ken@metaskills.net>
2016-01-10 11:15:50 -0500

    Finish cleaning out models for secondbase tests.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,31 +1,3 @@
 class Account < ActiveRecord::Base
 
-  attr_accessor :fail_validation
-  after_validation :check_fail_validation
-
-  attr_accessor :raise_rollback
-  after_validation :check_raise_rollback
-
-  class << self
-
-    def create_from_user!(user)
-      account = new email: user.email
-      account.fail_validation = user.account_fails_validation
-      account.raise_rollback = user.account_raise_rollback
-      account.save!
-    end
-
-  end
-
-
-  private
-
-  def check_fail_validation
-    errors.add :user, 'said to fail validation' if fail_validation
-  end
-
-  def check_raise_rollback
-    raise ActiveRecord::Rollback if raise_rollback
-  end
-
 end
